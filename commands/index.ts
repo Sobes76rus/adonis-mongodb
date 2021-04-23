2b94d410b8a4130ac03552285d0729fb63750518
Anton <myprojectorterrypratchett@gmail.com>
2021-04-24 00:11:23 +0300

    Fix

--- commands/index.ts
+++ commands/index.ts
@@ -2,6 +2,8 @@ const commands = [
   '@zakodium/adonis-mongodb/lib/commands/MongodbMakeMigration.js',
   '@zakodium/adonis-mongodb/lib/commands/MongodbMigrate.js',
   '@zakodium/adonis-mongodb/lib/commands/MongodbListMigrations.js',
+  '@zakodium/adonis-mongodb/lib/commands/MongodbMakeModel.js',
+  '@zakodium/adonis-mongodb/lib/commands/MongodbEnsureIndexes.js',
 ];
 
 export default commands;
